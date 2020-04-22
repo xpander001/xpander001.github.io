302f8b261664541f9d61d372ae114ef48fa21803
Ander Suarez Martinez <me@andersuarez.com>
2020-04-22 19:53:58 +0200

    Update

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Link from 'gatsby-link'
 import styled from 'styled-components'
 
 import '../styles/global.css.js'
-import {linkTextColor} from '../styles/variables.css.js'
+import { linkTextColor } from '../styles/variables.css.js'
 
 const MainSection = styled.section`
   height: 100%;
@@ -49,8 +49,8 @@ export default () => (
   <MainSection>
     <MainSectionContent>
       <Title>Ander Suárez Martínez</Title>
-      <Subtitle>Frontend developer at <InlineLink href="http://www.unbound.com">Unbound</InlineLink>. I build websites.</Subtitle>
-      <Text>Previously, frontend lead at <InlineLink href="http://www.jinnapp.com">Jinn</InlineLink>. I co-founded <InlineLink href="http://perpetuall.net/">Perpetuall</InlineLink>. I was born in Bilbao, and moved to the UK looking for new challenges. I like music & arts. I am a guitarist in a band called <InlineLink href="http://walnut.bandcamp.com">Walnut</InlineLink>.</Text>
+      <Subtitle>Senior Frontend Developer at <InlineLink href="http://www.unbound.com">Unbound</InlineLink>. I build websites.</Subtitle>
+      <Text>Previously, frontend lead at <InlineLink href="http://www.jinnapp.com">Jinn</InlineLink>. I co-founded <InlineLink href="http://perpetuall.net/">Perpetuall</InlineLink>. I was born in Bilbao, and moved to the UK looking for new challenges. I like music & arts. I play guitar for <InlineLink href="http://walnut.bandcamp.com">Walnut</InlineLink>.</Text>
       <Text>Find me at <InlineLink href="mailto:me@andersuarez.com">me@andersuarez.com</InlineLink></Text>
     </MainSectionContent>
   </MainSection>
